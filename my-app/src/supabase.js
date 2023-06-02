483902da8d01436363f801697ac18538381a338c
kevinc2465 <chenk5104@gmail.com>
2023-06-01 21:55:25 -0400

    pray#2

--- my-app/src/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const VITE_SUPABASE_URL=https://your-project-ref.supabase.co;
-const VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlkemlta3ZkdWVybGFuZmt3ZHNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM2NTE2NDQsImV4cCI6MTk5OTIyNzY0NH0.n8Y43N3HmFi5PbCFovhJYDbuOMIUhWVOv-UQx8uQcyc
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
--- sql-project/README.md
+++ sql-project/README.md
@@ -1,4 +1,4 @@
-# my-app
+# sql-project
 
 This template should help get you started developing with Vue 3 in Vite.
 
