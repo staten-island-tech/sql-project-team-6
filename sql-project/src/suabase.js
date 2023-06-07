1c46223eae1472c593ae6ebac7de5cf5a86ab298
kevinc2465 <chenk5104@gmail.com>
2023-06-07 13:13:44 -0400

    trying some router

--- sql-project/src/suabase.js
+++ sql-project/src/suabase.js
@@ -4,3 +4,8 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+//alternative way
+// const supabaseUrl = 'https://idzimkvduerlanfkwdse.supabase.co'
+// const supabaseKey = process.env.SUPABASE_KEY
+// const supabase = createClient(supabaseUrl, supabaseKey)
