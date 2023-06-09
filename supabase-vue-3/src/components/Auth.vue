d0d0d6e2071d9518ccfab94d8beae9974f339f4e
kevinc2465 <chenk5104@gmail.com>
2023-06-08 21:53:45 -0400

    added some css

--- supabase-vue-3/src/components/Auth.vue
+++ supabase-vue-3/src/components/Auth.vue
@@ -24,7 +24,7 @@ const handleLogin = async () => {
 </script>
 
 <template>
-  <form class="row flex-center flex" @submit.prevent="handleLogin">
+  <form class="form" @submit.prevent="handleLogin">
     <div class="col-6 form-widget">
       <h1 class="header">Your own personal management APP</h1>
       <p class="description">
@@ -51,4 +51,37 @@ const handleLogin = async () => {
   </form>
 </template>
 
-<style></style>
+<style>
+.header {
+  color: white;
+}
+
+.form {
+  background: lightblue;
+  width: 50%;
+  border-radius: 2rem;
+  text-align: center;
+}
+.background--custom {
+  background-color: #ffffff;
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  overflow: hidden;
+  z-index: -2;
+  top: 0;
+  left: 0;
+}
+canvas#canvas {
+  z-index: -1;
+  position: absolute;
+  width: 100%;
+  height: 60%;
+  transform: rotate(-12deg) scale(2) translateY(-30%);
+  --gradient-color-1: #b026ad;
+  --gradient-color-2: #3442d8;
+  --gradient-color-3: #d184e3;
+  --gradient-color-4: #25f77f;
+  --gradient-speed: 0.000006;
+}
+</style>
