c07931e148b9be7cbd4fc82e3b3460cae8071702
kevinc2465 <chenk5104@gmail.com>
2023-06-08 21:35:59 -0400

    finished code

--- supabase-vue-3/src/App.vue
+++ supabase-vue-3/src/App.vue
@@ -1,20 +1,20 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-import Account from './components/Account.vue'
-import Auth from './components/Auth.vue'
-import { supabase } from './supabase'
+import { onMounted, ref } from "vue";
+import Account from "./components/Account.vue";
+import Auth from "./components/Auth.vue";
+import { supabase } from "./supabase";
 
-const session = ref()
+const session = ref();
 
 onMounted(() => {
   supabase.auth.getSession().then(({ data }) => {
-    session.value = data.session
-  })
+    session.value = data.session;
+  });
 
   supabase.auth.onAuthStateChange((_, _session) => {
-    session.value = _session
-  })
-})
+    session.value = _session;
+  });
+});
 </script>
 
 <template>
